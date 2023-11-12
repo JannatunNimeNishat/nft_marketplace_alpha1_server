3f9ec5bb21b79c30b2e2317a2ce36ccae1fa8ce8
JannatunNimeNishat <nishatnime100@gmail.com>
2023-11-12 22:42:03 +0600

    delete an nft api added

--- controllers/nfts.controller.js
+++ controllers/nfts.controller.js
@@ -7,9 +7,12 @@ const getNft = (req, res) => {
 
 }
 
+const delete_An_nft = (req,res)=>{
+
+}
 
 
 
 
 
-module.exports = { createNft,getNft }
+module.exports = { createNft,getNft,delete_An_nft }
--- routes/nfts.route.js
+++ routes/nfts.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { createNft, getNft } = require('../controllers/nfts.controller');
+const { createNft, getNft, delete_An_nft } = require('../controllers/nfts.controller');
 const router = express.Router();
 
 
 
 router.post('/',createNft)
 router.get('/',getNft)
-
+router.delete('/',delete_An_nft)
 
 
 
